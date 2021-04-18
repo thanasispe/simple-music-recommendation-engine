2ced63c25ca963082d69f9d3366c42cf92feafec
thanasispe <79358998+thanasispe@users.noreply.github.com>
2021-04-18 12:04:44 +0300

    Initial commit

---
+++ ln.py
@@ -0,0 +1,26 @@
+age = int(input("Age? "))
+gender = str(input("Male or Female? "))
+
+
+if (gender.lower() == "male"):
+    gender = 1
+
+elif (gender.lower() == "female"):
+    gender = 0
+
+else:
+    quit(1)
+
+import pandas as pd 
+from sklearn.tree import DecisionTreeClassifier
+
+data = pd.read_csv("music.csv")
+
+X = data.drop(columns="genre")
+y = data['genre']
+
+model = DecisionTreeClassifier()
+model.fit(X,y)
+
+predictions = model.predict([[age, gender]])
+print("Do you like ", predictions[0])
